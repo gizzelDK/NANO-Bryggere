ee31649a4f3e50a7bb04f447452cd22e214898a4
Mikkel Gissel <ftv832@adm.ku.dk>
2022-08-29 18:04:46 +0200

    to urls til at prøve det med

--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -1,9 +1,15 @@
 // This file can be replaced during build by using the `fileReplacements` array.
 // `ng build` replaces `environment.ts` with `environment.prod.ts`.
 // The list of file replacements can be found in `angular.json`.
+const apiUrl = 'https://localhost:7142/api';
 
 export const environment = {
-  production: false
+  production: false,
+  environment: 'development',
+  
+  // API URL's
+  apiUrl: apiUrl,
+  brugerUrl: apiUrl + '/Bruger',
 };
 
 /*
