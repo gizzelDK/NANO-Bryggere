5c8dd96baa7e5ae63d174585fa53766b6ad2adf5
Simone Jenny <simonekh@live.dk>
2022-09-27 15:03:41 +0200

    "fixed" opskriftliste reiew: Victor

--- src/app/forum/forside/forside.component.ts
+++ src/app/forum/forside/forside.component.ts
@@ -16,13 +16,13 @@ import { OpretForumDialogBoxComponent } from '../opret-forum-dialog-box/opret-fo
   styleUrls: ['./forside.component.css']
 })
 export class ForsideComponent implements OnInit {
-  @Input() postOprettelse = { titel: '', indhold: '', brugerId: 0, forumId: 0 };
+  @Input() postOprettelse = { titel: '', indhold: '', brugerId: 0, forumId: 0 }; //post oprettelse fra html, put noget i html og sende videre til metoderne
   //, oprettet: ''
   @Input() postSvar = { titel: '', indhold: '', brugerId: 0, forumId: 0, postId: 0 };
   dialogRefOpretForum: MatDialogRef<OpretForumDialogBoxComponent>;
   dialogRefOpdaterForum: MatDialogRef<OpdaterForumDialogBoxComponent>;
   dialogRefOpdaterPost: MatDialogRef<OpdaterPostDialogBoxComponent>;
-  opretForm: any = new FormGroup({});
+  opretForm: any = new FormGroup({}); //ngonignite
   svarForm: any = new FormGroup({});
   forums: any;
   forum: Forum[];
@@ -30,7 +30,7 @@ export class ForsideComponent implements OnInit {
   postsId: any;
   brugerListe: any;
   postListe: any
-  endpointF = '/Fora';
+  endpointF = '/Fora'; //passer med api 
   endpointP = '/Posts';
   endpointB = '/Brugers';
   endpointR = '/Rolles';
@@ -40,7 +40,7 @@ export class ForsideComponent implements OnInit {
   postInfo: any;
   opdaterPost: any;
   opdaterForum: any;
-  id = this.actRoute.snapshot.params['id'];
+  id = this.actRoute.snapshot.params['id']; // tager id med videre hvor den er givet og sætte ud for den
   clickButton: boolean = true;
   clickBtnSvar = false;
   egenBrugerId: boolean;
@@ -77,8 +77,9 @@ export class ForsideComponent implements OnInit {
     this.onHentRolle();
   }
   onHentForum() {
-    return this.restApi.getDatas(this.endpointF).subscribe((dataF) => {
-      this.forums = dataF;
+    return this.restApi.getDatas(this.endpointF).subscribe((dataF) => { 
+      //henter data fra vores endpoint - liste med objekter retunres
+      this.forums = dataF; //data til this. forum til vores html i ngfor og printer ud
       // this.restApi.getDatas(this.endpointTØ).subscribe((data) => {
       //   // this.mineTagsList = data;
       //   // .filter((res:any) => {
@@ -116,44 +117,45 @@ export class ForsideComponent implements OnInit {
    onHentRolle(){
     this.restApi.getDatas(this.endpointR).subscribe(dataR =>{
       this.rolleListe = dataR
-      this.rolle = this.rolleListe.find((a:any) => a.level === 20)
+      this.rolle = this.rolleListe.find((a:any) => a.level === 20) //finder alle der er lig med 20(kUN)
     })
   }
 
-  onGodkendPost(id: any) {
-
-    this.postOprettelse.forumId = id;
-    this.postOprettelse.brugerId = this.brugerId;
+  onGodkendPost(id: any) { //svar besked 
+ 
+    this.postOprettelse.forumId = id; //giver id til objeckt og ngfor kører obejkt igennem og viser
+    this.postOprettelse.brugerId = this.brugerId; //giver brugerid så vi ved hvem der har oprettet det
      console.log(this.postOprettelse.brugerId);
      console.log('postInfo...', this.postOprettelse);
     this.restApi.createData(this.postOprettelse, this.endpointP).subscribe((dataP) => {
       console.log('postInfo...',dataP);
-      this.postOprettelse.indhold = '';
+      this.postOprettelse.indhold = ''; //gør tom igen /refresh side.... 
       this.postOprettelse.titel = '';
       this.ngOnInit();
     });
   }
-
+//bliver ikek brugt!!!!!!!
   onSvarToggle() {
-    this.clickBtnSvar = !this.clickBtnSvar;
+    this.clickBtnSvar = !this.clickBtnSvar; 
   }
 
   onSvarPost(forumId: any, postId: any) {
     this.postSvar.forumId = forumId;
     this.postSvar.brugerId = this.brugerId;
-    this.postSvar.postId = postId;
+    this.postSvar.postId = postId; //hvilkn post man svarer til med ids
     this.restApi.createData(this.postSvar, this.endpointP).subscribe((dataP) => {
       this.postSvar.indhold = '';
-      this.postSvar.titel = '';
+      this.postSvar.titel = ''; 
       this.ngOnInit();
     });
   }
 
   onVisPost(id: any) {
-    this.clickButton = false;
+    this.clickButton = false; 
     // this.brugerId2=JSON.parse(localStorage.getItem('brugerId2') || '{}');
     // this.listPosts= this.posts.filter((res:any) => res.forumId === id && (res.brugerId2 === this.brugerId2 || res.brugerId1 === this.brugerId1))
     this.posts = this.postListe.filter((res: any) => res.forumId === id);
+    //vis posts. tjekker efter id om de passer
   }
 
   onSletForum(id: any) {
@@ -180,6 +182,7 @@ export class ForsideComponent implements OnInit {
     }
     else {
       this.forums = this.forums.filter((res: any) => {
+        //tjekker vores obj om det passer så den laver ikke et api kald hver gang - tjekker gamle lister
         return res.titel.toLowerCase().match(this.searchkey.toLowerCase());
       })
     }
@@ -188,7 +191,7 @@ export class ForsideComponent implements OnInit {
   onOpdaterPost(id: any) {
     this.restApi.getData(id, this.endpointP).subscribe(dataP => {
       if (this.brugerId === dataP.brugerId || this.rolle ===20) {
-        localStorage.setItem('postId', JSON.stringify(id));
+        localStorage.setItem('postId', JSON.stringify(id)); //JSON = nummer og strings , gør til json object og smides ud igen som før
         const dialogConfig = new MatDialogConfig();
         dialogConfig.disableClose = true;
         dialogConfig.autoFocus = true;
